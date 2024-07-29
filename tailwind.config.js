8e6e69c2123604a3631d70e489e60e64ca37ff5f
nimcau <165478977+nimcau@users.noreply.github.com>
2024-07-30 01:52:29 +0300

    commited.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 //eslint-disable-next-line
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+
     fontFamily:{sans:"Robot Mono, monospace"},
 
     extend: {
